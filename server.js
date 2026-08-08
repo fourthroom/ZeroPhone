3df6a22b1b88eafec39e2a5775ca7f4a3ff0402f
fourthroom <eric.eugene.scott@fourthroom.org>
2026-08-08 07:11:59 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
-const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Health Check
 app.get('/health', (req, res) => {
   res.status(200).send('Signaling server is healthy');
 });
@@ -18,8 +20,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: '*',
-    methods: ['GET', 'POST'],
-  },
+    methods: ['GET', 'POST']
+  }
 });
 
 io.on('connection', (socket) => {
@@ -47,6 +49,7 @@ io.on('connection', (socket) => {
     io.emit('user-left', { peerId: socket.id });
   });
 });
+
 // Webhook for incoming SMS replies from Twilio
 app.post('/api/incoming-sms', (req, res) => {
   const fromNumber = req.body.From;
@@ -54,11 +57,13 @@ app.post('/api/incoming-sms', (req, res) => {
 
   console.log(`Incoming SMS from ${fromNumber}: ${bodyText}`);
 
-  // Broadcast to all connected clients via Socket.io
-  io.emit('sms-reply', { from: fromNumber, message: bodyText });
+  if (typeof io !== 'undefined') {
+    io.emit('sms-reply', { from: fromNumber, message: bodyText });
+  }
 
   res.type('text/xml').send('<Response></Response>');
 });
+
 // Endpoint to handle sending SMS from web page
 app.post('/api/send-sms', async (req, res) => {
   const { to, message } = req.body;
@@ -86,6 +91,7 @@ app.post('/api/send-sms', async (req, res) => {
     console.error('Twilio Error:', error);
     res.status(500).json({ success: false, error: error.message });
   }
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
